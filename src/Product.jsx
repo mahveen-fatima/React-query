05cd360387f967c66e9f42c9e903e2594e06fff3
mahveen-fatima <mahveenfatima2224@gmail.com>
2024-07-10 14:46:40 +0530

    new commit

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useParams } from 'react-router-dom';
+import axios from "axios";
 
 const product = () => {
 
     const params = useParams();
 
-    console.log(params);
+    // console.log(params);
+
+    // Mutations
+
+    const mutation = useMutation({
+        mutationFn: (newProduct) => {
+          return axios.put(`https://dummyjson.com/products/${params.productId}`, newProduct)
+        },
+      })
 
     const fetchProduct = async () => {
         const response = await fetch(`https://dummyjson.com/product/${params.productId}`);
@@ -34,8 +43,26 @@ const product = () => {
             return <h3>Error: {error.message}</h3> 
         }
 
+        if(mutation.isLoading) {
+            return <h1>updating...</h1>
+        }
 
-  return <div>Product: {product.title}</div>
+        if(mutation.isError) {
+            return <h3>Error while updating: {mutation.error.message}</h3> 
+        }
+
+  return <>
+  <div>Product: {product.title}</div>
+
+          <button
+            onClick={() => {
+              mutation.mutate({title: 'updated product' })
+            }}
+          >
+            Create Product
+          </button>
+  </>
+  
 }
 
 export default product
